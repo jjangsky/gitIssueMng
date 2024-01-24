5a4e1f661ec42435bf9970e8a5ef1680478a67ac
jjangsky <dbcks4646@gmail.com>
2024-01-25 00:09:15 +0900

    이슈 생성 에디터 추가

---
+++ issue-management-app/app/issues/new/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { Button, TextField, TextArea } from '@radix-ui/themes';
+import SimpleMDE from 'react-simplemde-editor';
+import 'easymde/dist/easymde.min.css';
+import React from 'react';
+
+const NewIssuePage = () => {
+    return (
+        <div className="max-w-xl space-y-3">
+            <TextField.Root>
+                <TextField.Input placeholder="Title" />
+            </TextField.Root>
+            <SimpleMDE placeholder="Description" />
+            <Button>Create New Issue</Button>
+        </div>
+    );
+};
+
+export default NewIssuePage;
