59b5245d30d4bdc5ec29443ec2e91b9870956bfa
YouchanKim <dbcks4646@eco.co.kr>
2024-01-25 00:09:15 +0900

    이슈 생성 에디터 추가

--- issue-management-app/app/issues/page.tsx
+++ issue-management-app/app/issues/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Button } from '@radix-ui/themes';
 
 const IssuesPage = () => {
-    return <div>page 여기</div>;
+    return <Button>new Issue</Button>;
 };
 
 export default IssuesPage;
