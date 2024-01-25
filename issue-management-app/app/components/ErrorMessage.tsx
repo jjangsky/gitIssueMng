51ce381dacfaa27fb20386e8a07a9097fbe7b13b
jjangsky <dbcks4646@gmail.com>
2024-01-25 23:52:16 +0900

    테스트 커밋

--- issue-management-app/app/components/ErrorMessage.tsx
+++ issue-management-app/app/components/ErrorMessage.tsx
@@ -3,7 +3,7 @@ import { Text } from '@radix-ui/themes';
 
 // PropsWithChildren 사용 시, 인터페이스를 직접적으로 정의하는 수고를 덜어줌
 const ErrorMessage = ({ children }: PropsWithChildren) => {
-    if (!children) return null; // children이 없을 경우 null 반환 -> 즉, 오류가 있을 때만 동작 처리
+    if (!children) return null; // children이 없을 경우 null 반환함 -> 즉, 오류가 있을 때만 동작 처리
     return (
         <Text color="red" as="p">
             {children}
