6fc058c92a9d7ee22e59ef112c33538cb73cd5d7
YouchanKim <dbcks4646@eco.co.kr>
2024-01-28 16:24:51 +0900

    이슈 상세페이지

--- issue-management-app/app/issues/[id]/page.tsx
+++ issue-management-app/app/issues/[id]/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import prisma from '@/prisma/client';
 import { notFound } from 'next/navigation';
+import { Card, Flex, Heading } from '@radix-ui/themes';
+import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 
 interface Props {
     params: { id: string };
@@ -21,10 +23,14 @@ const IssueDetailPage = async ({ params }: Props) => {
     if (!issue) notFound();
     return (
         <div>
-            <p>{issue.title}</p>
-            <p>{issue.description}</p>
-            <p>{issue.status}</p>
-            <p>{issue.createdAt.toDateString()}</p>
+            <Heading>{issue.title}</Heading>
+            <Flex my="2" className="space-x-3">
+                <IssueStatusBadge status={issue.status} />
+                <p>{issue.createdAt.toDateString()}</p>
+            </Flex>
+            <Card>
+                <p>{issue.description}</p>
+            </Card>
         </div>
     );
 };
