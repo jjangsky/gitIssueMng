1796b53f4d84fa2033075bd7d9fb27760bc1101c
jjangsky <dbcks4646@gmail.com>
2024-01-29 00:21:08 +0900

    이슈 다이얼로그 생성

--- issue-management-app/app/issues/[id]/DeleteIssueButton.tsx
+++ issue-management-app/app/issues/[id]/DeleteIssueButton.tsx
@@ -1,8 +1,29 @@
-import { Button } from '@radix-ui/themes';
-import React from 'react';
+'use client';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
-    return <Button color="red"> Delete Button</Button>;
+    // AlertDialog는 브라우저 API를 사용하기 때문에 Client 컴포넌트로 전환 필요
+    return (
+        <AlertDialog.Root>
+            <AlertDialog.Trigger>
+                <Button color="red">Delete Button</Button>
+            </AlertDialog.Trigger>
+            <AlertDialog.Content>
+                <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+                <AlertDialog.Description> 정말 삭제하시길 원합니까?</AlertDialog.Description>
+                <Flex mt="4" gap="3">
+                    <AlertDialog.Cancel>
+                        <Button variant="soft" color="gray">
+                            Cancel
+                        </Button>
+                    </AlertDialog.Cancel>
+                    <AlertDialog.Action>
+                        <Button color="red">Delete</Button>
+                    </AlertDialog.Action>
+                </Flex>
+            </AlertDialog.Content>
+        </AlertDialog.Root>
+    );
 };
 
 export default DeleteIssueButton;
