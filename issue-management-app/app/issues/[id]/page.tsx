187c293684473c2e64bb2cf76f0260602c625db9
YouchanKim <dbcks4646@eco.co.kr>
2024-01-28 20:55:23 +0900

    수정페이지 생성

--- issue-management-app/app/issues/[id]/page.tsx
+++ issue-management-app/app/issues/[id]/page.tsx
@@ -7,6 +7,8 @@ import ReactMarkDown from 'react-markdown';
 import delay from 'delay';
 import { Pencil2Icon } from '@radix-ui/react-icons';
 import Link from 'next/link';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
 
 interface Props {
     params: { id: string };
@@ -32,20 +34,10 @@ const IssueDetailPage = async ({ params }: Props) => {
         // initial -> 초기값, md -> 중간 사이즈 이상일 경우, 2개의 컬럼으로 표시
         <Grid columns={{ initial: '1', md: '2' }} gap="5">
             <Box>
-                <Heading>{issue.title}</Heading>
-                <Flex my="2" className="space-x-3">
-                    <IssueStatusBadge status={issue.status} />
-                    <p>{issue.createdAt.toDateString()}</p>
-                </Flex>
-                <Card className="prose" mt="4">
-                    <ReactMarkDown>{issue.description}</ReactMarkDown>
-                </Card>
+                <IssueDetails issue={issue} />
             </Box>
             <Box>
-                <Button>
-                    <Pencil2Icon />
-                    <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-                </Button>
+                <EditIssueButton issueId={issue.id} />
             </Box>
         </Grid>
     );
