d753fc48ec10be3f41d50bf12a4bc48677b1e3ce
jjangsky <dbcks4646@gmail.com>
2024-01-28 18:11:48 +0900

    로딩 스켈레톤 추가(상세페이지 및 생성 페이지)

--- issue-management-app/app/issues/new/loading.tsx
+++ issue-management-app/app/issues/new/loading.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import { Box } from '@radix-ui/themes';
 
 const LoadingIssueNewPage = () => {
-    return <div>loading</div>;
+    return (
+        <Box className="max-w-xl">
+            <Skeleton />
+            <Skeleton height="20rm" />
+        </Box>
+    );
 };
 
 export default LoadingIssueNewPage;
