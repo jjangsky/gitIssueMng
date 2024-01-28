c0fc2575259bf174c292e176b8f803348a136ce8
YouchanKim <dbcks4646@eco.co.kr>
2024-01-28 18:58:42 +0900

    임포트 리팩토링

---
+++ issue-management-app/app/components/index.tsx
@@ -0,0 +1,7 @@
+export { default as Link } from './Link';
+export { default as ErrorMessage } from './ErrorMessage';
+export { default as IssueStatusBadge } from './IssueStatusBadge';
+export { default as Spinner } from './Spinner';
+// import 와 export 한번에 구현
+
+// 해당 폴더에서 components 폴더 내의 모든 파일을 불러옴, 즉 해당 폴더만 보면 댐
