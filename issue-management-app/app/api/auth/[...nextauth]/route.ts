4c3ea647c77fdefcd1f65641c30ea9d7ac74c72e
jjangsky <dbcks4646@gmail.com>
2024-01-29 23:57:28 +0900

    구글 프로바이더 추가

--- issue-management-app/app/api/auth/[...nextauth]/route.ts
+++ issue-management-app/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,15 @@
 import NextAuth from 'next-auth/next';
+import GoogleProvider from 'next-auth/providers/google';
 
 const handler = NextAuth({
-    providers: [],
+    providers: [
+        GoogleProvider({
+            // 환경변수를 사용하여 클라이언트 ID와 시크릿 키를 설정
+            // ts에서 환경변수에 있는 값을 인지시키기 위해서는 끝에 느낌표를 붙여야 함
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+    ],
 });
 
 export { handler as GET, handler as POST };
