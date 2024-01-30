35ee5b6ae6f553bc0d335a21633da8ef6d69ad0f
YouchanKim <dbcks4646@eco.co.kr>
2024-01-30 22:33:19 +0900

    구글 로그인 설정

--- issue-management-app/app/api/auth/[...nextauth]/route.ts
+++ issue-management-app/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,10 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from '@/prisma/client';
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
 
 const handler = NextAuth({
+    adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
             // 환경변수를 사용하여 클라이언트 ID와 시크릿 키를 설정
@@ -10,6 +13,11 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
     ],
+    session: {
+        // authjs는 adapter를 사용하는 경우 jwt로 전환되는데
+        // jwt는 구글과 같은 auth provider를 사용할 수 없음 -> 별도로 세션 프로퍼티 설정 필요
+        strategy: 'jwt',
+    },
 });
 
 export { handler as GET, handler as POST };
