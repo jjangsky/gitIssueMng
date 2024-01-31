594474cdd22c9a25f6fe6880196156a2b7f070fa
YouchanKim <dbcks4646@eco.co.kr>
2024-01-31 23:22:00 +0900

    유저 조회 api 수정

--- issue-management-app/app/api/users/route.ts
+++ issue-management-app/app/api/users/route.ts
@@ -2,10 +2,11 @@ import prsima from '@/prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
-    const users = await prisma?.user.findMany({
+    const users = await prsima?.user.findMany({
         orderBy: {
             name: 'asc',
         },
     });
+
     return NextResponse.json(users);
 }
