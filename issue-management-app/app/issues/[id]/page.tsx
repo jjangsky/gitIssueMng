f241dd2b6eb7e021085a280e761e97b3df48c27a
YouchanKim <dbcks4646@eco.co.kr>
2024-02-03 14:05:13 +0900

    이슈담당자 api 적용

--- issue-management-app/app/issues/[id]/page.tsx
+++ issue-management-app/app/issues/[id]/page.tsx
@@ -45,7 +45,7 @@ const IssueDetailPage = async ({ params }: Props) => {
             {session && (
                 <Box>
                     <Flex direction="column" gap="4">
-                        <AssigneeSelect />
+                        <AssigneeSelect issue={issue} />
                         <EditIssueButton issueId={issue.id} />
                         <DeleteUssueButton issueId={issue.id} />
                     </Flex>
