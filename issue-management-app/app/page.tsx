46d0618eba5661075ab964d7c6d98e4090c17e2e
jjangsky <dbcks4646@gmail.com>
2024-02-05 22:54:21 +0900

    메인 대시보드 생성

--- issue-management-app/app/page.tsx
+++ issue-management-app/app/page.tsx
@@ -1,5 +1,6 @@
+import LatestIssues from './LatestIssues';
 import Pagination from './components/Pagination';
 
 export default function Home({ searchParams }: { searchParams: { page: string } }) {
-    return <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />;
+    return <LatestIssues />;
 }
